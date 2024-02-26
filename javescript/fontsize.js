2d324c04362118e0ce9f4631e7c69ad3571cd00f
Wong Chun Hong <wchjohn2@gmail.com>
2024-02-26 17:39:24 +0800

    Add about autism page

--- javescript/fontsize.js
+++ javescript/fontsize.js
@@ -1,3 +1,5 @@
+//can define 3 set of font size, laptop, table and mobile
+
 document.getElementById("header_small_A").onclick = function() {
   document.documentElement.style.setProperty('--font-size-extra-small', "20px");
   document.documentElement.style.setProperty('--font-size-small', "28px");
@@ -6,8 +8,8 @@ document.getElementById("header_small_A").onclick = function() {
 }
 
 document.getElementById("header_big_A").onclick = function() {
-  document.documentElement.style.setProperty('--font-size-extra-small', "24px");
-  document.documentElement.style.setProperty('--font-size-small', "32px");
-  document.documentElement.style.setProperty('--font-size-medium', "40px");
-  document.documentElement.style.setProperty('--font-size-large', "52px");
+  document.documentElement.style.setProperty('--font-size-extra-small', "22px");
+  document.documentElement.style.setProperty('--font-size-small', "30px");
+  document.documentElement.style.setProperty('--font-size-medium', "38px");
+  document.documentElement.style.setProperty('--font-size-large', "50px");
 }
