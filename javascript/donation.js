b6a68e00213a3f841dbecfaffeb60ea85981e8d2
Wong Chun Hong <wchjohn2@gmail.com>
2024-03-03 15:55:18 +0800

    Add comments

--- javascript/donation.js
+++ javascript/donation.js
@@ -1,8 +1,10 @@
+//highlight the box
 var selectElement = function(element) {
   element.style.backgroundColor = "#0D6792";
   element.style.color = "#ffffff";
 }
 
+//unhighlight the box
 var unselectElement = function(element) {
   element.style.backgroundColor = "#ffffff";
   element.style.color = "#000000";
@@ -10,11 +12,13 @@ var unselectElement = function(element) {
 
 
 
+//highlight the monthly box and unhighligh other boxes
 document.getElementById("donation_choice_monthly").onclick = function() {
   selectElement(document.getElementById("donation_choice_monthly"));
   unselectElement(document.getElementById("donation_choice_oneoff"));
 }
 
+//highlight the oneoff box and unhighligh other boxes
 document.getElementById("donation_choice_oneoff").onclick = function() {
   selectElement(document.getElementById("donation_choice_oneoff"));
   unselectElement(document.getElementById("donation_choice_monthly"));
@@ -22,6 +26,7 @@ document.getElementById("donation_choice_oneoff").onclick = function() {
 
 
 
+//highlight the one hundred box, unhighlight other boxes and clear text for entering amount
 document.getElementById("donation_choice_onehundred").onclick = function() {
   selectElement(document.getElementById("donation_choice_onehundred"));
   unselectElement(document.getElementById("donation_choice_fivehundred"));
@@ -29,6 +34,7 @@ document.getElementById("donation_choice_onehundred").onclick = function() {
   document.getElementById("donation_choice_customamount").innerText = "";
 }
 
+//highlight the five hundred box, unhighlight other boxes and clear text for entering amount
 document.getElementById("donation_choice_fivehundred").onclick = function() {
   selectElement(document.getElementById("donation_choice_fivehundred"));
   unselectElement(document.getElementById("donation_choice_onehundred"));
@@ -36,6 +42,7 @@ document.getElementById("donation_choice_fivehundred").onclick = function() {
   document.getElementById("donation_choice_customamount").innerText = "";
 }
 
+//highlight the one thousand box, unhighlight other boxes and clear text for entering amount
 document.getElementById("donation_choice_onethousand").onclick = function() {
   selectElement(document.getElementById("donation_choice_onethousand"));
   unselectElement(document.getElementById("donation_choice_fivehundred"));
@@ -43,14 +50,16 @@ document.getElementById("donation_choice_onethousand").onclick = function() {
   document.getElementById("donation_choice_customamount").innerText = "";
 }
 
+//unhighlight other boxes
 document.getElementById("donation_choice_customamount").oninput = function() {
   unselectElement(document.getElementById("donation_choice_onehundred"));
   unselectElement(document.getElementById("donation_choice_fivehundred"));
   unselectElement(document.getElementById("donation_choice_onethousand"));
 }
 
+
+
+//show an alert for thank you the donation
 document.getElementById("donation_submit_button").onclick = function() {
   alert("Thank You for Your Donation");
 }
-
-
--- javascript/fontsize.js
+++ javascript/fontsize.js
@@ -1,5 +1,6 @@
 //can define 3 set of font size, laptop, table and mobile
 
+//set the font size to small for laptop, tablet and mobile resolution
 document.getElementById("header_small_A").onclick = function() {
   document.documentElement.style.setProperty('--font-size-laptop-extra-small', "20px");
   document.documentElement.style.setProperty('--font-size-laptop-small', "28px");
@@ -7,6 +8,7 @@ document.getElementById("header_small_A").onclick = function() {
   document.documentElement.style.setProperty('--font-size-laptop-large', "48px");
 }
 
+//set the font size to large for laptop, tablet and mobile resolution
 document.getElementById("header_big_A").onclick = function() {
   document.documentElement.style.setProperty('--font-size-laptop-extra-small', "22px");
   document.documentElement.style.setProperty('--font-size-laptop-small', "30px");
